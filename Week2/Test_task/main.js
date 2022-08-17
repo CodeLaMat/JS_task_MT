b80d5963d406c62cf4738cc1da39cac109f54f4e
CodeLaMat <eyvaz.alishov@gmail.com>
2022-08-17 14:37:35 +0300

    fixing folder

---
+++ Week2/Test_task/main.js
@@ -0,0 +1,15 @@
+function Myfunction ()
+let voteable;
+  let age = Number(document.getElementById("age").value);
+
+if (age<18) {
+    alert ("too young")
+  
+} if (age<27) {
+    alert ("Right age for the military service")
+
+}
+
+if (age<41) {
+    alert ("You are in reserve")
+}
